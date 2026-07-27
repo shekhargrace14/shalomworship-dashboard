b2a8a7fa020e24ec65e307769ea3c210050a6eb1
shekhar <shekhargrace14@gmail.com>
2026-07-27 12:03:17 +0530

    fix(CORS):middleware added to handle url and cors issue

--- app/api/channel/service.ts
+++ app/api/channel/service.ts
@@ -1,41 +1,6 @@
 import { prisma } from '@/lib/prisma';
 import { getCurrentUserService } from '@/lib/services/auth.service';
 
-// export async function getChannelsService(
-//   mine: boolean
-// ) {
-
-//   if (mine) {
-
-//     const user = await getCurrentUserService();
-
-//     const memberships =
-//       await prisma.channelMember.findMany({
-//         where: {
-//           userId: user.id,
-//           status: "ACTIVE",
-//         },
-//         include: {
-//           channel: true,
-//         },
-//       });
-
-//     return memberships.map((membership) => ({
-//       ...membership.channel,
-//       role: membership.role,
-//     }));
-//   } else {
-
-//     const channel = prisma.channel.findMany({
-//       orderBy: {
-//         createdAt: "desc",
-//       },
-//     })
-//     return channel
-//   }
-
-// }
-
 export async function getChannelsService(mine: boolean) {
   if (mine) {
     const user = await getCurrentUserService();
